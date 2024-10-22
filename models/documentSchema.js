3c81cecf3e5a085a29186c712fb378fcf5d4c8e8
SUBID DAS <itsmesubid@gmail.com>
2024-10-22 21:26:14 +0530

    Refactor documentSchema to include a required driver field

--- models/documentSchema.js
+++ models/documentSchema.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const documentSchema = new mongoose.Schema(
   {
+    driver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Driver",
+      required: true,
+    },
     name: {
       type: String,
       required: true,
