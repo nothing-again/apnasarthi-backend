29232483d4e776f0e5016ca7534492495b75ace4
SUBID DAS <itsmesubid@gmail.com>
2024-10-22 21:24:44 +0530

    Refactor driver and vehicle models

---
+++ models/documentSchema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Document = mongoose.model("Document", documentSchema);
+
+export default Document;
--- models/vehicleSchema.js
+++ models/vehicleSchema.js
@@ -1,47 +1,41 @@
 import mongoose from "mongoose";
 
 const vehicleSchema = new mongoose.Schema({
-    driver: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Driver",
-        required: true,
-    },
+  driver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Driver",
+    required: true,
+  },
 
-    vehicleType: {
-        type: String,
-        required: true,
-    },
+  vehicleType: {
+    type: String,
+    required: true,
+  },
 
-    carModel: {
-        type: String,
-        required: true,
-    },
+  carModel: {
+    type: String,
+    required: true,
+  },
 
-    carYear: {
-        type: Number,
-        required: true,
-    },
+  carYear: {
+    type: Number,
+    required: true,
+  },
 
-    registrationNumber: {
-        type: String,
-        required: true,
-    },
+  registrationNumber: {
+    type: String,
+    required: true,
+  },
 
-    insuranceNumber: {
-        type: String,
-        required: true,
-    },
+  insuranceNumber: {
+    type: String,
+    required: true,
+  },
 
-    documents: {
-        registrationCertificate: {
-            type: String,
-            required: true,
-        },
-        insurance: {
-            type: String,
-            required: true,
-        },
-    },
+  insuranceExpiry: {
+    type: Date,
+    required: true,
+  },
 });
 
 const Vehicle = mongoose.model("Vehicle", vehicleSchema);
