118738515eb4231044b2f0ccaace793941d9003c
HARIOM-JHA01 <jhahariom040@gmail.com>
2024-10-22 23:21:33 +0530

    otp service added

--- services/otpService.js
+++ services/otpService.js
@@ -1,32 +1,33 @@
-import dotenv from "dotenv";
 import axios from "axios";
-
+import dotenv from "dotenv";
 dotenv.config();
+const sendOTP = async (toNumber, otp) => {
+    try {
+        const response = await axios.post(
+            "https://www.fast2sms.com/dev/bulkV2",
+            {
+                route: "v3",
+                sender_id: "TXTIND",
+                message: `Your OTP is ${otp}`,
+                language: "english",
+                flash: 0,
+                numbers: toNumber,
+            },
+            {
+                headers: {
+                    authorization: process.env.FAST2SMS_AUTH_KEY,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
 
-const authKey = process.env.MSG91_AUTH_KEY;
-
-const sendOtp = async (mobileNumber) => {
-  const options = {
-    method: "POST",
-    url: "https://control.msg91.com/api/v5/otp",
-    params: {
-      otp_expiry: "10",
-      template_id: "6717b386d6fc052b50602df2",
-      mobile: mobileNumber,
-      authkey: authKey,
-      realTimeResponse: "1",
-    },
-    headers: { "Content-Type": "application/JSON" },
-    data: '{\n  "Param1": "value1",\n  "Param2": "value2",\n  "Param3": "value3"\n}',
-  };
-
-  try {
-    const { data } = await axios.request(options);
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+        console.log("OTP sent successfully:", response.data);
+    } catch (error) {
+        console.error(
+            "Error sending OTP:",
+            error.response ? error.response.data : error.message
+        );
+    }
 };
 
-// Send OTP to the specified number
-sendOtp("+919523566969", "1234");
+export { sendOTP };
