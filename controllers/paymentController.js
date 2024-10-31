93a8b5fa6e8ec864367c61a90445a2c385ab14a9
SUBID DAS <itsmesubid@gmail.com>
2024-10-31 23:03:57 +0530

    Refactor paymentController.js to round up the amount before creating an order

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,7 +3,7 @@ import { createOrder } from "../services/razorpayService.js";
 export const generateOrderId = async (req, res) => {
   try {
     const { amount, currency } = req.body;
-    const orderId = await createOrder(amount, currency);
+    const orderId = await createOrder(Math.ceil(amount), currency);
     res.status(200).json({ orderId });
   } catch (error) {
     console.log({ error });
