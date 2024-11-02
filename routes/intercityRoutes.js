866f9ab9fd80edacf271653d2c83913ce8527206
SUBID DAS <itsmesubid@gmail.com>
2024-11-02 14:10:38 +0530

    Refactor intercityRoutes.js for consistent code formatting and add /available route

--- routes/intercityRoutes.js
+++ routes/intercityRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {
-    getIntercityByRiderId,
-    getIntercityById,
-    createIntercity,
-    updateIntercity,
-    deleteIntercity,
-    getIntercityByDriverId,
-    getIntercityByStatus,
-    getEstimatedFare,
+  getIntercityByRiderId,
+  getIntercityById,
+  createIntercity,
+  updateIntercity,
+  deleteIntercity,
+  getIntercityByDriverId,
+  getIntercityByStatus,
+  getEstimatedFare,
 } from "../controllers/intercityController.js";
 
 const router = express.Router();
@@ -16,11 +16,12 @@ router.route("/rider/:riderId").get(getIntercityByRiderId);
 router.route("/driver/:driverId").get(getIntercityByDriverId);
 router.route("/status/:status").get(getIntercityByStatus);
 router.route("/estimate").post(getEstimatedFare);
+router.route("/available").get(() => console.log("Hello"));
 router
-    .route("/:id")
-    .get(getIntercityById)
-    .put(updateIntercity)
-    .delete(deleteIntercity);
+  .route("/:id")
+  .get(getIntercityById)
+  .put(updateIntercity)
+  .delete(deleteIntercity);
 router.route("/").post(createIntercity);
 
 export default router;
